4faf0098c1d1e8908bb4832a86732805f6e69bf9
JoannaLigenza <ligenzajoanna@gmail.com>
2019-10-27 14:39:53 +0100

    menu and main components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-
+import './index.css';
+import Menu from './components/menu.js';
+import Main from './components/main.js';
 
 function App() {
     return (
         <div className="app">
-           
+            <Menu />
+            <Main />
         </div>
   );
 }
