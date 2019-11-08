476bb284900fab5368e0b1e4d3ad383d40b1f318
JoannaLigenza <ligenzajoanna@gmail.com>
2019-11-08 19:00:52 +0100

    comments added

--- src/components/lessonNavigation.js
+++ src/components/lessonNavigation.js
@@ -10,18 +10,23 @@ const LessonNavigation = (props) => {
     const speakRate = getSettings.settings.speakRate;
     return (
         <div className="navigation">
+            {/* Arrow Left */}
             <div className="navigation--left" onClick={() => props.changeWord("prev")} 
+                // if it is first word in lesson then hide arrow left, else display it
                 style={{visibility: props.words.currentWord <= props.displayFrom ? "hidden" : "visible", display: props.displayLeftArrow === "yes" ? "block" : "none"}}>
                 Prev
             </div>
+            {/* Speaker Icon */}
             <div style={{display: props.displayLoudSpeaker === "yes" ? "block" : "none"}}>
                 <img src={speaker} alt="speaker icon - press and listen" className="speaker-icon"
                         onClick={() => props.speakWord !== undefined ? speak(props.speakWord, language, speakRate) : null}
                         style={{position: "absolute"}}/>
             </div>
+            {/* Go to Test Button */}
             <div className="button button--goToTest" style={{display: props.words.currentWord >= props.displayTo ? "block" : "none"}}
                 onClick={() => {
                     if (props.rightAnswer !== undefined) {
+                        // if answer is correct go to next overlap, else play wrong sound
                         if (props.rightAnswer === "greenColor") {
                             props.setContent.changeContent("setContentInOverlap", props.goToOverlap);
                             props.visibility.changeVisibility("activeOverlap", props.goToOverlap);
@@ -29,15 +34,18 @@ const LessonNavigation = (props) => {
                             playSound("wrongSound");
                         }
                     } else {
+                        // in writing section regardless to answer go to next overlap
                         props.setContent.changeContent("setContentInOverlap", props.goToOverlap);
                         props.visibility.changeVisibility("activeOverlap", props.goToOverlap);
                     }
                 }}>
                 {props.buttonText}
             </div>
+            {/* Arrow Right */}
             <div className="navigation--right" onClick={() => {
                     // if props.rightAnswer or props.getSettings === undefined then this component is used by lessonComponent, else it is used by reading, listening, writing or test component
                     if (props.rightAnswer !== undefined) {
+                        // if answer is correct not play antything and go to next word, else play wrong sound
                         if (props.rightAnswer === "greenColor") {
                             props.changeWord("next");
                         } else {
@@ -46,11 +54,12 @@ const LessonNavigation = (props) => {
                     } else {
                         props.changeWord("next");
                     }
-
+                    // reset actual answers
                     if (props.getSettings !== undefined) {
                         props.getSettings.setSettings("actualAnswers", "");
                     }
                 }}
+                // if it is the last word in lesson then hide arrow right, else display it
                 style={{visibility: props.words.currentWord >= props.displayTo ? "hidden" : "visible"}}>
                 Next
             </div>
